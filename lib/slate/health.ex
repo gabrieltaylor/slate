f77691aa3e72c904e09c9cabbb64b6688eede378
Troy Martin <troy@devato.com>
2020-08-22 17:36:51 -0400

    Remove plug

---
+++ lib/slate/health.ex
@@ -0,0 +1,17 @@
+defmodule Slate.Health do
+  @moduledoc """
+  System Health checks
+  """
+
+  @health_check_error_code 422
+
+  def checks do
+    [
+      %PlugCheckup.Check{name: "NOOP", module: __MODULE__, function: :noop_health}
+    ]
+  end
+
+  def error_code, do: @health_check_error_code
+
+  def noop_health, do: :ok
+end
