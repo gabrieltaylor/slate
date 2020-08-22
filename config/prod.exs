3eaf3bee55ef74ddb294ae985aef76ee2a8c2ce2
Troy Martin <troy@devato.com>
2020-08-22 15:50:20 -0400

    Add render yaml

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,7 @@
 use Mix.Config
 
 config :slate, SlateWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
-
-import_config "prod.secret.exs"
